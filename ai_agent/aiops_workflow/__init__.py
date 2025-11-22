bb98703ef33f73307f0d41596304773ae4f9610b
Nangmanful <89606590659@naver.com>
2025-11-22 12:18:24 +0900

    [add] route/._AIOPS기능구현

--- ai_agent/aiops_workflow/__init__.py
+++ ai_agent/aiops_workflow/__init__.py
@@ -1,19 +1,23 @@
 '''
 파일명: __init__.py
 최종 수정일: 2025-11-22
-버전: v04
+버전: v05
 파일 개요: AIops Workflow 패키지 초기화
 변경 이력:
 	- 2025-11-22: v01 - AIops Workflow 패키지 생성
 	- 2025-11-22: v02 - Hazard Batch Processor 추가
 	- 2025-11-22: v03 - Hazard Scheduler 추가 (독립적 실행)
 	- 2025-11-22: v04 - grid_processor 제거, 스케줄러 파일명 변경
+	- 2025-11-22: v05 - 확장성을 고려한 최소 구조 (batch/ 분리)
 '''
 
-from .probability_batch import ProbabilityBatchProcessor
-from .hazard_batch import HazardBatchProcessor
-from .probability_scheduler import ProbabilityScheduler
-from .hazard_scheduler import HazardScheduler
+# Batch 작업
+from .batch import (
+	ProbabilityBatchProcessor,
+	HazardBatchProcessor,
+	ProbabilityScheduler,
+	HazardScheduler
+)
 
 __all__ = [
 	'ProbabilityBatchProcessor',
